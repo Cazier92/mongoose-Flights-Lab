2a965da84351effceff99f509f98960e28911273
Cazier92 <bryce.cazier@gmail.com>
2023-01-10 16:30:49 -0800

    add tickets as embedded model within flights, viewed and added in the show view

--- controllers/flights.js
+++ controllers/flights.js
@@ -84,6 +84,25 @@ function update(req, res) {
   })
 }
 
+function createTicket(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    flight.tickets.push(req.body)
+    flight.save()
+    .then(() => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect(`/flights/${flight._id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
   index,
   newFlight as new,
@@ -92,4 +111,6 @@ export {
   show,
   edit,
   update,
+  createTicket,
+
 }
